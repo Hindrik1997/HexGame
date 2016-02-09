db2b76a132268c7e9b84cf1f8e0dfede0113d9b7
Hindrik1997 <hindrik1997@hotmail.com>
2016-02-09 11:30:40 +0100

    Added minimal additions

---
+++ HEX/WindowFunctions.cpp
@@ -0,0 +1,43 @@
+#include "WindowFunctions.h"
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+		{
+			if (MessageBox(NULL, L"Are you sure you want to exit?", L"Really?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+			{
+				DestroyWindow(hwnd);
+			}
+		}
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+HWND InitializeWindow(string WindowClassName,string WindowTitle ,HINSTANCE hInstance, int ShowWnd, int width, int height, bool windowed)
+{
+	WNDCLASSEX wc;
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = StringToWideString(WindowClassName).c_str();
+	wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
+	
+
+
+}
